af5e21884d657c70607f41b8ea9f145689cf9255
brmcdoug <brmcdoug@cisco.com>
2022-12-06 12:52:23 -0800

    startup

--- startup.sh
+++ startup.sh
@@ -6,12 +6,12 @@
 
 echo "starting setup: " >> /home/ubuntu/startup.log
 
-./xr-compose -f docker-compose-6-node.yml  -li localhost/ios-xr:7.8.1.18I
-sudo sysctl -p
-echo "sleeping for 10 seconds to let images build" >> /home/ubuntu/startup.log
-sleep 10
+#./xr-compose -f docker-compose-6-node.yml  -li localhost/ios-xr:7.8.1.18I
+#sudo sysctl -p
+#echo "sleeping for 10 seconds to let images build" >> /home/cisco/startup.log
+#sleep 10
 
-docker network ls | awk -F': ' '/srv6_dcloud_lab_mgmt /{print $0}' > mgt_br.txt
+docker network ls | awk -F': ' '/srv6_dcloud_lab_mgmt /{print $0}' > /home/cisco/mgt_br.txt
 mgt_br=$( head -n 1 mgt_br.txt | cut -c 1-12 )
 
 echo "$mgt_br"
