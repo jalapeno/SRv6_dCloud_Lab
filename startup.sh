25b2a2b0c0b7f97ae2f333318892fa9e27532e2b
brmcdoug <brmcdoug@cisco.com>
2022-12-09 12:30:44 -0800

    compose

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@
 ./xr-compose -f docker-compose-7-node.yml -i localhost/ios-xr:7.7.1
 
 sed -i 's/linux:xr-100/linux:eth0/g' docker-compose.yml
-sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose.yml
+sed -i 's/xrd01-host: null/macvlan0: null/g' docker-compose.yml
 
 docker-compose up --detach
 
