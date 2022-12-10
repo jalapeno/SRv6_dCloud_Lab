547fd3740ce5d8c79647658a9e33b6fa32a6d1dd
brmcdoug <brmcdoug@cisco.com>
2022-12-10 17:09:14 +0000

    compose

--- lab_0/start_lab_0.sh
+++ lab_0/start_lab_0.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-../xr-compose -f lab_0-topology.yml -i localhost/ios-xr:7.7.1
+#../xr-compose -f lab_0-topology.yml -i localhost/ios-xr:7.7.1
 
-sed -i 's/linux:xr-130/linux:eth0/g' docker-compose.yml
-sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose.yml
+#sed -i 's/linux:xr-130/linux:eth0/g' docker-compose.yml
+#sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose.yml
 
-docker-compose up --detach
+docker-compose -f docker-compose.yml up --detach
