22ee932fae1f5bf581674fa3f3e3854793cd8ca1
brmcdoug <brmcdoug@cisco.com>
2022-12-12 10:36:55 -0800

    macvlan1

--- lab_0/start_lab_0.sh
+++ lab_0/start_lab_0.sh
@@ -7,7 +7,7 @@
 #sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose.yml
 
 # macvlan for xrd07
-#sed -i 's/linux:xr-180/linux:eth0/g' docker-compose.yml
-#sed -i 's/xrd07-gi0: null/macvlan0: null/g' docker-compose.yml
+#sed -i 's/linux:xr-180/linux:eth1/g' docker-compose.yml
+#sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose.yml
 
 docker-compose -f docker-compose.yml up --detach
