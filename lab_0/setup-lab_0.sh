61278708e8da365b659eeccf13a8abdafe128333
brmcdoug <brmcdoug@cisco.com>
2022-12-17 18:19:25 -0800

    echo

--- lab_0/setup-lab_0.sh
+++ lab_0/setup-lab_0.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
+echo "building compose-lab_0.yml"
 ../xr-compose -f lab_0-topology.yml -o docker-compose-lab_0.yml -i ios-xr/xrd-control-plane:7.8.1
 
+echo "macvlan edits"
 # macvlan for xrd01 
 sed -i 's/linux:xr-120/linux:eth0/g' docker-compose-lab_0.yml
 sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_0.yml
@@ -10,4 +12,5 @@ sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_0.yml
 sed -i 's/linux:xr-180/linux:eth1/g' docker-compose-lab_0.yml
 sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_0.yml
 
+echo "launching topology"
 docker-compose -f docker-compose-lab_0.yml up --detach
