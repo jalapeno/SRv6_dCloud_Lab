cfb0aa52029c56266997e415ee890a5ea9ecf815
Bruce McDougall <brmcdoug@cisco.com>
2022-12-19 11:17:38 -0800

    Update setup-lab_0.sh

--- lab_0/setup-lab_0.sh
+++ lab_0/setup-lab_0.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 
+### run cleanup script to eliminate any stale docker network instances or volumes 
+
+./cleanup-lab_0.sh
+
 ### Uncomment these five lines if you need to rebuild lab_0's docker-compose-lab_0.yml file:
 
 # ../xr-compose -f lab_0-topology.yml -o docker-compose-lab_0.yml -i ios-xr/xrd-control-plane:7.8.1
@@ -8,8 +12,12 @@
 # sed -i 's/linux:xr-180/linux:eth0/g' docker-compose-lab_0.yml
 # sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_0.yml
 
+### launch topology via docker-compose
+
 echo "launching topology"
 docker-compose -f docker-compose-lab_0.yml up --detach
+
+### reset sysctl to allow routing over docker bridge instances
 
 echo "sudo sysctl -p"
 sudo sysctl -p
