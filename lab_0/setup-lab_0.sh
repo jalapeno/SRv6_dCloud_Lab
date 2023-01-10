473bf41b464cb4bdaaae41fd941de597de8f2048
brmcdoug <brmcdoug@cisco.com>
2023-01-10 12:06:15 -0800

    update compose

--- lab_0/setup-lab_0.sh
+++ lab_0/setup-lab_0.sh
@@ -4,13 +4,21 @@
 
 ./cleanup-lab_0.sh
 
-### Uncomment these five lines if you need to rebuild lab_0's docker-compose-lab_0.yml file:
+### Uncomment these lines if you need to rebuild lab_0's docker-compose-lab_0.yml file:
 
 # ../xr-compose -f lab_0-topology.yml -o docker-compose-lab_0.yml -i ios-xr/xrd-control-plane:7.8.1
+
 # sed -i 's/linux:xr-120/linux:eth0/g' docker-compose-lab_0.yml
 # sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_0.yml
+
+# sed -i 's/linux:xr-130/linux:eth1/g' docker-compose-lab_0.yml
+# sed -i 's/xrd01-gi3: null/macvlan3: null/g' docker-compose-lab_0.yml
+
 # sed -i 's/linux:xr-180/linux:eth0/g' docker-compose-lab_0.yml
 # sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_0.yml
+
+# sed -i 's/linux:xr-190/linux:eth1/g' docker-compose-lab_0.yml
+# sed -i 's/xrd07-gi3: null/macvlan2: null/g' docker-compose-lab_0.yml
 
 ### launch topology via docker-compose
 
--- lab_3/setup-lab_3.sh
+++ lab_3/setup-lab_3.sh
@@ -4,13 +4,21 @@
 
 ./cleanup-lab_3.sh
 
-### Uncomment these five lines if you need to rebuild lab_3's docker-compose-lab_3.yml file:
+### Uncomment these lines if you need to rebuild lab_5's docker-compose-lab_5.yml file:
 
-#../xr-compose -f lab_3-topology.yml -o docker-compose-lab_3.yml -i ios-xr/xrd-control-plane:7.8.1
+# ../xr-compose -f lab_3-topology.yml -o docker-compose-lab_3.yml -i ios-xr/xrd-control-plane:7.8.1
+
 # sed -i 's/linux:xr-120/linux:eth0/g' docker-compose-lab_3.yml
 # sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_3.yml
+
+# sed -i 's/linux:xr-130/linux:eth1/g' docker-compose-lab_3.yml
+# sed -i 's/xrd01-gi3: null/macvlan3: null/g' docker-compose-lab_3.yml
+
 # sed -i 's/linux:xr-180/linux:eth0/g' docker-compose-lab_3.yml
 # sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_3.yml
+
+# sed -i 's/linux:xr-190/linux:eth1/g' docker-compose-lab_3.yml
+# sed -i 's/xrd07-gi3: null/macvlan2: null/g' docker-compose-lab_3.yml
 
 echo "launching topology"
 docker-compose -f docker-compose-lab_3.yml up --detach
