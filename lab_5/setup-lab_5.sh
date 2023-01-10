8c594959a9d2a5d70a0e25035cfb26f42f823e8a
robermur <robermur@cisco.com>
2023-01-10 08:18:10 -0800

    modified lab1 and lab 2

--- lab_5/setup-lab_5.sh
+++ lab_5/setup-lab_5.sh
@@ -2,18 +2,18 @@
 
 ### run cleanup script to eliminate any stale docker network instances or volumes 
 
-./cleanup-lab_4.sh
+./cleanup-lab_5.sh
 
-### Uncomment these five lines if you need to rebuild lab_4's docker-compose-lab_4.yml file:
+### Uncomment these five lines if you need to rebuild lab_5's docker-compose-lab_5.yml file:
 
-#../xr-compose -f lab_4-topology.yml -o docker-compose-lab_4.yml -i ios-xr/xrd-control-plane:7.8.1
-# sed -i 's/linux:xr-120/linux:eth0/g' docker-compose-lab_4.yml
-# sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_4.yml
-# sed -i 's/linux:xr-180/linux:eth0/g' docker-compose-lab_4.yml
-# sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_4.yml
+#../xr-compose -f lab_5-topology.yml -o docker-compose-lab_5.yml -i ios-xr/xrd-control-plane:7.8.1
+# sed -i 's/linux:xr-120/linux:eth0/g' docker-compose-lab_5.yml
+# sed -i 's/xrd01-gi0: null/macvlan0: null/g' docker-compose-lab_5.yml
+# sed -i 's/linux:xr-180/linux:eth0/g' docker-compose-lab_5.yml
+# sed -i 's/xrd07-gi0: null/macvlan1: null/g' docker-compose-lab_5.yml
 
 echo "launching topology"
-docker-compose -f docker-compose-lab_4.yml up --detach
+docker-compose -f docker-compose-lab_5.yml up --detach
 
 echo "generate docker bridge files for tcpdump utility"
 ../util/nets.sh
