9eea48728d225925603a6f19ddfc3fb1c569b87f
brmcdoug <brmcdoug@cisco.com>
2023-01-16 15:26:51 -0800

    qdisc

--- util/nets.sh
+++ util/nets.sh
@@ -9,7 +9,14 @@ docker network ls | awk -F': ' '/xrd01-gi1-xrd02-gi0 /{print $0}' > net.txt
 netinstance=$( head -n 1 net.txt | cut -c 1-12 )
 echo br-"$netinstance" > /home/cisco/SRv6_dCloud_Lab/util/xrd01-xrd02
 echo br-"$netinstance"
-rm net.txt
+
+brctl show | grep br-"$netinstance" > br.txt 
+veth=$(rev br.txt | cut -c -11 | rev ) > veth.txt
+sudo tc qdisc add dev $veth root netem delay 10000
+
+#rm net.txt
+#rm br.txt
+rm veth.txt
 
 docker network ls | awk -F': ' '/xrd01-gi2-xrd05-gi0 /{print $0}' > net.txt
 netinstance=$( head -n 1 net.txt | cut -c 1-12 )
