5cd5ed7ce7db28b7f0a2d1d8a45fb0ec645437c7
robermur <robermur@cisco.com>
2023-01-24 11:19:38 -0800

    Added set_latency.py

---
+++ lab_7/netservice/set_latency.py
@@ -0,0 +1,21 @@
+# This program is used to set latency values in both the lab topology router links and update the values in AranagoDb
+import argparse
+
+# Handle cli options passed in
+parser = argparse.ArgumentParser(
+    prog = 'Lab Latency Creater',
+        description = 'Sets latency on a single link in the lab topology',
+        epilog = 'set_latency.py -l <a..i> -ms <0 -300>')
+parser.add_argument("-l", help="link identifier values A through I")
+parser.add_argument("-ms", help="latency on link in ms")  
+
+args = parser.parse_args()
+
+# Check that the required input arguments were passed in
+if not args.l or not args.ms:
+    print("Required input elements encapsulation type, input file, and/or service type were not entered")
+    print("set_latency.py -l <a..i> -ms <0 -300>")
+    exit()
+
+f = open(file)
+print (f)
