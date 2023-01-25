ec1a03f41d7d8ff242ac971c3f9f31817878d1c7
robermur <robermur@cisco.com>
2023-01-25 09:07:53 -0800

    updated latency python

--- lab_7/netservice/set_latency.py
+++ lab_7/netservice/set_latency.py
@@ -1,24 +1,56 @@
-# This program is used to set latency values in both the lab topology router links and update the values in AranagoDb
 import argparse
+import subprocess
+import re
 
 # Handle cli options passed in
+link_options = ['A','B','C','D','E','F','G','H','I']
 parser = argparse.ArgumentParser(
     prog = 'Lab Latency Creater',
         description = 'Sets latency on a single link in the lab topology',
         epilog = 'set_latency.py -l <a..i> -ms <0 -300>')
-parser.add_argument("-l", help="link identifier values A through I")
-parser.add_argument("-ms", help="latency on link in ms")  
+parser.add_argument("-l", choices=link_options,required=True, help="link identifier values A through I")
+parser.add_argument("-ms", choices=range(1,300), type=int, required=True, help="latency on link in ms")  
 
 args = parser.parse_args()
+# Map link cli input to file descriptor
+file_dict={
+    'A':'xrd01-xrd02',
+    'B':'xrd01-xrd05',
+    'C':'xrd02-xrd06',
+    'D':'xrd05-xrd04',
+    'E':'xrd02-xrd03',
+    'F':'xrd03-xrd04',
+    'G':'xrd05-xrd06',
+    'H':'xrd04-xrd07',
+    'I':'xrd06-xrd07'
+}
 
-# Check that the required input arguments were passed in
-#if not args.l or not args.ms:
-#    print("Required input elements encapsulation type, input file, and/or service type were not entered")
-#    print("set_latency.py -l <a..i> -ms <0 -300>")
-#    exit()
+file = '../../util' + file_dict.get(args.l)
+print (file)
 
-file='../../util/xrd01-xrd02'
-
+# Open and read in the router link file
 with open(file, 'r') as file:
-    data = file.read().rstrip()
-print (data)
+    bridge_id = file.read().rstrip()
+print (bridge_id)
+
+# Run bridge control and find assocaiated interface to the bridge_id
+
+# using the Popen function to execute the
+# command and store the result in temp.
+# it returns a tuple that contains the 
+# data and the error if any.
+temp1 = subprocess.Popen(['brctl', 'show'], stdout = subprocess.PIPE)
+temp2 = subprocess.Popen(['grep', bridge_id],stdin=temp1.stdout, stdout=subprocess.PIPE)
+    
+# we use the communicate function
+# to fetch the output
+a = str(temp2.communicate())
+
+# splitting the output so that
+# we can parse them line by line
+b = re.sub(r'\\t|\\n', ',', a)
+c = b.split(",")
+# search the list for veth interface
+for i in c:
+ if i[0:4] == "veth":
+   interface = i
