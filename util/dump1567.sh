09a7eded2b6144da412fe142c9a0a3c921392322
brmcdoug <brmcdoug@cisco.com>
2023-03-17 21:57:32 -0700

    backgroung

--- util/dump1567.sh
+++ util/dump1567.sh
@@ -2,27 +2,43 @@
 
 line=$(head -n 1 xrd01-xrd05)
 echo "sudo tcpdump -ni $line"
-sudo tcpdump -ni $line
+sudo tcpdump -ni $line &
 sleep 4
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
-echo "stop tcpdump"
+echo "
+
+stopping tcpdump
+
+"
 
 line=$(head -n 1 xrd05-xrd06)
-echo "sudo tcpdump -ni $line"
-sudo tcpdump -ni $line
+echo "sudo tcpdump -ni $line
+
+"
+sudo tcpdump -ni $line &
 sleep 4
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
-echo "stop tcpdump"
+echo "
+
+stopping tcpdump
+
+"
 
 line=$(head -n 1 xrd06-xrd07)
-echo "sudo tcpdump -ni $line"
-sudo tcpdump -ni $line
+echo "sudo tcpdump -ni $line
+
+"
+sudo tcpdump -ni $line &
 sleep 4
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
-echo "stop tcpdump"
+echo "
+
+stopping tcpdump
+
+"
