ff31ccaa8293b574f0468d0751b4b75608eb2518
brmcdoug <brmcdoug@cisco.com>
2023-03-17 22:20:27 -0700

    format

--- util/tcpdump-xrd01-05-06-07.sh
+++ util/tcpdump-xrd01-05-06-07.sh
@@ -1,35 +1,33 @@
 #/bin/bash
 
 line=$(head -n 1 xrd01-xrd05)
-echo "running tcpdump on xrd01 to xrd05 link
+echo "
+running tcpdump on xrd01 to xrd05 link
 
 "
 echo "sudo tcpdump -ni $line"
 sudo tcpdump -ni $line &
-sleep 4
+sleep 3
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
 echo "
-
 stopping tcpdump
 
 "
 
 line=$(head -n 1 xrd05-xrd06)
 echo "running tcpdump on xrd05 to xrd06 link
-
 "
 echo "sudo tcpdump -ni $line
 
 "
 sudo tcpdump -ni $line &
-sleep 4
+sleep 3
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
 echo "
-
 stopping tcpdump
 
 "
@@ -41,12 +39,11 @@ echo "sudo tcpdump -ni $line
 
 "
 sudo tcpdump -ni $line &
-sleep 4
+sleep 3
 pid=$(ps -e | pgrep tcpdump)  
 echo $pid  
 sudo kill -9 $pid
 echo "
-
 stopping tcpdump
 
 "
