72f67c0703c89b10c91c966083bbda706bef7bad
brmcdoug <brmcdoug@cisco.com>
2023-03-17 21:59:27 -0700

    format

--- util/dump1567.sh
+++ util/dump1567.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
 line=$(head -n 1 xrd01-xrd05)
+echo "running tcpdump on xrd01 to xrd05 link
+
+"
 echo "sudo tcpdump -ni $line"
 sudo tcpdump -ni $line &
 sleep 4
@@ -14,6 +17,9 @@ stopping tcpdump
 "
 
 line=$(head -n 1 xrd05-xrd06)
+echo "running tcpdump on xrd05 to xrd06 link
+
+"
 echo "sudo tcpdump -ni $line
 
 "
@@ -27,7 +33,9 @@ echo "
 stopping tcpdump
 
 "
+echo "running tcpdump on xrd06 to xrd07 link
 
+"
 line=$(head -n 1 xrd06-xrd07)
 echo "sudo tcpdump -ni $line
 
