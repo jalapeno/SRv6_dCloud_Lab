81e94e8f0ddf074b2d53c9225329cebda28333c8
robermur <robermur@cisco.com>
2023-12-06 13:00:01 -0800

    adding ansible startup scripts

---
+++ ansible/ansible-lab-intialize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "startup script launched at: " > /home/cisco/deploy.log
+
+date >> /home/cisco/deploy.log
+whoami >> /home/cisco/deploy.log
+
+ansible-playbook -e "ansible_user=cisco ansible_ssh_pass=cisco123 ansible_sudo_pass=cisco123" \
+  /home/cisco/SRv6_dCloud_Lab/ansible/dcloud_startup_playbook.yml -v >> /home/cisco/deploy.log
