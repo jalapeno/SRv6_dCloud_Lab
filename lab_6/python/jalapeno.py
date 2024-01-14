db22e3c33d17e451555d82f68c0282860b135d9b
brmcdoug <brmcdoug@cisco.com>
2024-01-14 10:31:01 -0800

    argparse

--- lab_6/python/jalapeno.py
+++ lab_6/python/jalapeno.py
@@ -11,9 +11,9 @@ def main():
         prog = 'Jalapeno client',
         description = 'takes command line input and calls path calculator functions',
         epilog = 'jalapeno.py -f <json file> -e <sr or srv6> -s <ll, lu, ds, or gp> ')
-    parser.add_argument("-e", help="encapsulation type <sr> <srv6>", type=str.lower)
-    parser.add_argument("-f", help="json file with src, dst, parameters", type=str.lower)  
-    parser.add_argument("-s", help="requested network service: ll = low_latency, lu = least_utilized, ds = data_sovereignty, gp = get_paths)", type=str.lower)
+    parser.add_argument("-e", default=False, help="encapsulation type <sr> <srv6>")
+    parser.add_argument("-f", help="json file with src, dst, parameters")  
+    parser.add_argument("-s", help="requested network service: ll = low_latency, lu = least_utilized, ds = data_sovereignty, gp = get_paths)")
     args = parser.parse_args()
 
     encap = args.e
