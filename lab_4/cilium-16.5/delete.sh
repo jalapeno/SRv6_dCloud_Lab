50233a23ce62602e6784f30c474c5546c7af5112
brmcdoug <brmcdoug@cisco.com>
2025-01-14 09:45:44 -0800

    sh scripts

--- lab_4/cilium-16.5/delete.sh
+++ lab_4/cilium-16.5/delete.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-kubectl delete -f bgp-config.yaml 
+kubectl delete -f bgp-cluster-config.yaml 
 kubectl delete -f bgp-peer-config.yaml 
-kubectl delete -f bgp-locator-advert.yaml 
+kubectl delete -f bgp-vrf-config.yaml 
+kubectl delete -f bgp-locator-advert-config.yaml 
 kubectl delete -f srv6-locator-pool.yaml 
 kubectl delete -f vrf-carrots.yaml 
