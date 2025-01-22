768e69c7949707a85f1ecf8aff4eaeed16a0b014
brmcdoug <brmcdoug@cisco.com>
2025-01-22 08:15:42 -0800

    cleanup

--- lab_4/cilium/delete.sh
+++ lab_4/cilium/delete.sh
@@ -8,4 +8,3 @@ kubectl delete -f 05-bgp-vrf.yaml
 kubectl delete -f 06-srv6-locator-pool.yaml 
 kubectl delete -f 07-vrf-carrots.yaml 
 kubectl delete -f 08-vrf-radish.yaml 
-kubectl delete -f 09-carrot-to-radish.yaml 
