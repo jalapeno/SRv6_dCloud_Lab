6fcaa1c733e413337188f40adfcef098fb70c4cb
brmcdoug <brmcdoug@cisco.com>
2025-01-22 08:27:01 -0800

    gitpull service

--- util/gitpull.sh
+++ util/gitpull.sh
@@ -4,3 +4,10 @@ export HOME=/home/cisco
 cd /home/cisco/SRv6_dCloud_Lab
 git config --global --add safe.directory /home/cisco/SRv6_dCloud_Lab
 git pull
+
+cd /home/cisco/srctl
+git pull
+
+cd /home/cisco
+chown cisco:cisco -R /home/cisco/SRv6_dCloud_Lab
+chown cisco:cisco -R /home/cisco/srctl
