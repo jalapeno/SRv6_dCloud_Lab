45364e1c517ed717f8c9170bbfd902c27b7e1f94
brmcdoug <brmcdoug@cisco.com>
2025-01-22 21:41:34 -0800

    jalapeno cleanup

--- util/jalapeno_cleanup.py
+++ util/jalapeno_cleanup.py
@@ -3,9 +3,9 @@ import json
 
 user = "root"
 pw = "jalapeno"
-dbname = "jalapeno"
+dbname = "system"
 
 client = ArangoClient(hosts='http://198.18.128.101:30852')
 db = client.db(dbname, username=user, password=pw)
 
-db.delete_database(dbname)
+db.delete_database(jalapeno)
