36fd8a67b5b7ba6cb6b0ad31c32bd79e1e98d4f3
brmcdoug <brmcdoug@cisco.com>
2025-01-22 22:39:28 -0800

    jalapeno cleanup

--- util/jalapeno_cleanup.py
+++ util/jalapeno_cleanup.py
@@ -1,4 +1,5 @@
 from arango import ArangoClient
+from arango.exceptions import CollectionDeleteError, GraphDeleteError
 import json
 
 user = "root"
@@ -8,24 +9,28 @@ dbname = "jalapeno"
 client = ArangoClient(hosts='http://198.18.128.101:30852')
 db = client.db(dbname, username=user, password=pw)
 
+# Collections to delete
+collections = [
+    'igp_node', 'bgp_node', 'igp_domain', 'ibgp_prefix_v4', 'ibgp_prefix_v6',
+    'ebgp_prefix_v4', 'ebgp_prefix_v6', 'inet_prefix_v4', 'inet_prefix_v6',
+    'srv6_local_sids', 'igpv4_graph', 'igpv6_graph', 'ipv4_graph', 'ipv6_graph',
+    'hosts'
+]
 
-db.delete_collection('igp_node')
-db.delete_collection('bgp_node')
-db.delete_collection('igp_domain')
-db.delete_collection('ibgp_prefix_v4')
-db.delete_collection('ibgp_prefix_v6')
-db.delete_collection('ebgp_prefix_v4')
-db.delete_collection('ebgp_prefix_v6')
-db.delete_collection('inet_prefix_v4')
-db.delete_collection('inet_prefix_v6')
-db.delete_collection('srv6_local_sids')
-db.delete_collection('igpv4_graph')
-db.delete_collection('igpv6_graph')
-db.delete_collection('ipv4_graph')
-db.delete_collection('ipv6_graph')
-db.delete_graph('ipv4_graph')
-db.delete_graph('ipv6_graph')
-db.delete_graph('igpv4_graph')
-db.delete_graph('igpv6_graph')
-db.delete_collection('hosts')
+# Graphs to delete
+graphs = ['ipv4_graph', 'ipv6_graph', 'igpv4_graph', 'igpv6_graph']
+
+# Delete collections
+for collection in collections:
+    try:
+        db.delete_collection(collection)
+    except CollectionDeleteError:
+        print(f"Collection {collection} not found, skipping")
+
+# Delete graphs
+for graph in graphs:
+    try:
+        db.delete_graph(graph)
+    except GraphDeleteError:
+        print(f"Graph {graph} not found, skipping")
 
