f71230068517f49c6d499039f5fad6f52286479f
Robert Murphy <robermur@cisco.com>
2025-02-03 13:11:43 -0800

    Update gitpull.sh
    Adding change to make the git pull override local state.

--- util/gitpull.sh
+++ util/gitpull.sh
@@ -3,11 +3,13 @@
 export HOME=/home/cisco
 cd /home/cisco/SRv6_dCloud_Lab
 git config --global --add safe.directory /home/cisco/SRv6_dCloud_Lab
-git pull
+git fetch origin
+git reset --hard origin/main 
 
 cd /home/cisco/srctl
 git config --global --add safe.directory /home/cisco/srctl
-git pull
+git fetch origin
+git reset --hard origin/main 
 
 cd /home/cisco
 chown cisco:cisco -R /home/cisco/SRv6_dCloud_Lab
