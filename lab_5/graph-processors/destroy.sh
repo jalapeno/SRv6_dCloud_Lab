0831a5d6689257081daf4177cc5278969e710644
brmcdoug <brmcdoug@cisco.com>
2025-02-04 10:53:48 -0800

    script

--- lab_5/graph-processors/destroy.sh
+++ lab_5/graph-processors/destroy.sh
@@ -4,3 +4,5 @@ kubectl delete -f igp-graph.yaml
 kubectl delete -f ipv4-graph.yaml
 kubectl delete -f ipv6-graph.yaml
 kubectl delete -f srv6-localsids.yaml
+kubectl delete -f ui-deployment.yaml
+kubectl delete -f api-deployment.yaml
