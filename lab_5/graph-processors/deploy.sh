56b90c41cd2879a3128360313215024f43b9c8d6
brmcdoug <brmcdoug@cisco.com>
2025-02-17 18:30:43 -0800

    api and ui svc

--- lab_5/graph-processors/deploy.sh
+++ lab_5/graph-processors/deploy.sh
@@ -6,6 +6,8 @@ sleep 10
 kubectl apply -f ipv4-graph.yaml
 kubectl apply -f ipv6-graph.yaml
 kubectl apply -f srv6-localsids.yaml
-kubectl apply -f ui-deployment.yaml
 kubectl apply -f api-deployment.yaml
+kubectl apply -f api-svc.yaml
+kubectl apply -f ui-deployment.yaml
+kubectl apply -f ui-svc.yaml
 
