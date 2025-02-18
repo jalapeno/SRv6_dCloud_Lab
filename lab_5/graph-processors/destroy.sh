e54c282afdd6e24aa65989791b5becd09b6fc9f9
brmcdoug <brmcdoug@cisco.com>
2025-02-17 18:31:26 -0800

    typo

--- lab_5/graph-processors/destroy.sh
+++ lab_5/graph-processors/destroy.sh
@@ -6,3 +6,5 @@ kubectl delete -f ipv6-graph.yaml
 kubectl delete -f srv6-localsids.yaml
 kubectl delete -f ui-deployment.yaml
 kubectl delete -f api-deployment.yaml
+kubectl delete -f ui-svc.yaml
+kubectl delete -f api-svc.yaml
