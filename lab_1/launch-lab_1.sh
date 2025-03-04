409de1814cb086f9831b4fbf171184e40dd8d4fd
brmcdoug <brmcdoug@cisco.com>
2025-03-03 22:52:01 -0800

    bridges

--- lab_1/launch-lab_1.sh
+++ lab_1/launch-lab_1.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 echo "adding bridges"
-sudo brctl addbr xrd01-amsterdam1
-sudo brctl addbr xrd01-amsterdam2
-sudo brctl addbr xrd02-berlin1
+sudo brctl addbr amsterdam1
+sudo brctl addbr amsterdam2
+sudo brctl addbr berlin1
+sudo ip link set amsterdam1 up
+sudo ip link set amsterdam2 up
+sudo ip link set berlin1 up
 
 echo "starting VMs"
 virsh start amsterdam
