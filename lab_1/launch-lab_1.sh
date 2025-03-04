f1fcafbda18b2a6ff696c7ce9c92630ccf77253b
brmcdoug <brmcdoug@cisco.com>
2025-03-03 22:46:11 -0800

    script

--- lab_1/launch-lab_1.sh
+++ lab_1/launch-lab_1.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+echo "adding bridges"
 sudo brctl addbr xrd01-amsterdam1
 sudo brctl addbr xrd01-amsterdam2
 sudo brctl addbr xrd02-berlin1
 
+echo "starting VMs"
+virsh start amsterdam
+virsh start berlin
+
+echo "deploying lab"
 sudo clab deploy -t lab_1-topology.yml
+
+
 
 
